ab342c5da2ddca108655a64f392c31429972ed24
Ammar204 <ashabbir197@gmail.com>
2021-01-18 21:07:41 +0500

    js

---
+++ index.js
@@ -0,0 +1,25 @@
+function clickHandler(eve){
+
+    // const btn = eve.target
+    
+
+    // let classFields = document.getElementsByClassName('val')
+    // console.log(classFields)
+    // const values = Object.keys(classFields).map(key => classFields[key].value)
+    // if(values[1].length < 8){
+    //     btn.classList.remove('btn-primary')
+    // btn.classList.add('btn-danger')
+    // }else{
+
+    //     btn.classList.remove('btn-danger')
+    //     btn.classList.add('btn-primary') 
+    // }
+
+
+    console.log("cklic")
+    const box = document.getElementsByClassName('box')[0]
+    box.classList.toggle('active')
+        
+
+
+}
---
+++ myscript.js
@@ -0,0 +1,17 @@
+const todos = []
+
+function clicked(){
+
+    // const todo = document.getElementById('todoItem').value
+    // todos.push(todo)
+    // const list = document.getElementById("todoList")
+
+    // let temp = ""
+    // todos.forEach(todo => temp += `<li>${todo}</li> <button> delete </button>` )
+    // list.innerHTML = temp;
+
+
+    const sp = document.getElementById('sp')
+    sp.nextElementSibling.style.background = "red"
+    console.log(sp.nextElementSibling)
+};
